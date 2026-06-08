6abcf5f074ad7a7dab1ecedf366adaff3f7fa600
1tc-selode <oseletye@gmail.com>
2026-06-08 12:34:02 +0200

    security

---
+++ portfolio/functions/_middleware.js
@@ -0,0 +1,21 @@
+export async function onRequest(context) {
+	const { request, next } = context;
+	const url = new URL(request.url);
+
+	if (url.pathname.toLowerCase().startsWith('/images/')) {
+		const referer = request.headers.get('Referer') || '';
+		const host = url.hostname;
+
+		// Allow if request comes from the same site
+		const isInternal = referer && (
+			referer.includes(host) ||
+			referer.includes('seletyeodett')
+		);
+
+		if (!isInternal) {
+			return Response.redirect('https://' + host + '/', 302);
+		}
+	}
+
+	return next();
+}
